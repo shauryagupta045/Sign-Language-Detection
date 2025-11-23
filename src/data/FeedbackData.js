ff2e1c4c42b5bbbf60e84d15b81b3143e796d916
Shaurya Gupta <guptashaurya0728@gmail.com>
2025-11-23 23:11:32 +0530

    adding vercel.json file

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,15 +1,15 @@
 export const userFeedback = [
     {
-        title: "Harsh Bharadwaj",
+        title: "Rohit",
         text: "The sign language AI web app is an innovative and user-friendly tool that accurately detects hand gestures and translates them into corresponding signs. Its ensures reliable performance, making it an excellent resource for communication and education.",
-    
+
     },
     {
-        title: "Shaurya Gupta",
+        title: "Lewis",
         text: "The user experinece of the application is very smooth and easy to understand. Used this application to try out different signs in ASL and detection was smooth and prompt.",
     },
     {
-        title: "Kunal Tiwari",
+        title: "Romain",
         text: "The advancement in the field of sign language is really helpful for the deaf  and dumb this eliminates the need of a middle by just using the website, and last but not the least the UI is super smooth and user-friendly.",
 
     }
--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "signlanguagedetection-ff760.firebaseapp.com",
-  projectId: "signlanguagedetection-ff760",
-  storageBucket: "signlanguagedetection-ff760.firebasestorage.app",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: "G-72PSGX9VB6"
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -17,5 +17,5 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-export { firebase, auth, db};
+export { firebase, auth, db };
 
